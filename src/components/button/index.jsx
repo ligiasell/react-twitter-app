c8144c4cdecddbf3ace7557abf86d5e7ac5b41d6
Lígia Sell <ligia.sell@hotmail.com>
2022-08-11 16:24:43 -0300

    create button and switch button

---
+++ src/components/button/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './styles.css'
+
+const Button = ({ onClick, children }) => {
+  return (
+    <button type="button" onClick={onClick} className="button">
+      <span>{children}</span>
+    </button>
+  )
+}
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+  children: PropTypes.node,
+}
+
+Button.defaultProps = {
+  onClick: () => {},
+  children: null,
+}
+
+export default Button
---
+++ src/components/button/switch-button/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './styles.css'
+
+const SwitchButton = ({ label, checked, onChange, id, name }) => (
+  <div className="switch-button">
+    <input type="checkbox" id="label" className="switch-input" checked={checked} onChange={onChange} name={name} data-id={id} />
+    <label htmlFor="label" className="switch-button-label">
+      {label}
+    </label>
+  </div>
+)
+
+SwitchButton.propTypes = {
+  label: PropTypes.string,
+  checked: PropTypes.bool,
+  onChange: PropTypes.func,
+  name: PropTypes.string,
+  id: PropTypes.number,
+}
+
+SwitchButton.defaultProps = {
+  label: '',
+  checked: false,
+  onChange: () => {},
+  name: '',
+  id: undefined,
+}
+
+export default SwitchButton
