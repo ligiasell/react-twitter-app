bd00769e70835b041d42d64c1625350a8895a4b7
Lígia Sell <ligia.sell@hotmail.com>
2022-08-18 17:09:05 -0300

    refactor modal, move header to layout

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -1,37 +1,94 @@
-import React from 'react'
-import ReactModal from 'react-modal'
-import PropTypes from 'prop-types'
+import React, { useState, useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 import Button from '../button'
+import TextArea from '../text-area'
+// import POSTS from '../../posts-data.json'
+import USERS from '../../users-data.json'
+import { LOGGED_USER_ID, MODAL_OVERLAY_STYLE, MODAL_BACKGROUND_STYLE } from '../../utils/constants'
 
 import './styles.css'
 
-const Modal = ({ isOpen, onClose, children }) => (
-  <ReactModal
-    className="modal"
-    isOpen={isOpen}
-    contentLabel="Dynamic content"
-    overlayClassName="overlay"
-    bodyOpenClassName="no-scroll"
-    ariaHideApp={false}
-    onRequestClose={onClose}
-  >
-    <div className="modal-close-button">
-      <Button onClick={onClose}>X</Button>
+const Modal = () => {
+  let navigate = useNavigate()
+  let userId = parseInt(useParams().id)
+  const [isFollowing, setIsFollowing] = useState(undefined)
+  const [selectedUser, setSelectedUser] = useState({})
+  console.log('selectedUser', selectedUser)
+
+  const handleCloseModal = (event) => {
+    event.stopPropagation()
+    navigate(-1)
+  }
+
+  const handleIsFollowing = () => {
+    const isLoggedUserFollowing = selectedUser.followers.includes(LOGGED_USER_ID)
+    if (isLoggedUserFollowing) {
+      const filteredFollowers = selectedUser.followers.filter((user) => user !== LOGGED_USER_ID)
+      setSelectedUser({
+        ...selectedUser,
+        followers: filteredFollowers,
+      })
+      setIsFollowing(false)
+    } else {
+      setSelectedUser((prevState) => ({
+        ...selectedUser,
+        followers: [...prevState.followers, LOGGED_USER_ID],
+      }))
+      setIsFollowing(true)
+    }
+  }
+
+  const handlePost = () => {
+    // TODO: Add post and close modal
+  }
+
+  useEffect(() => {
+    setSelectedUser(USERS.find((user) => user.id === userId))
+  }, [userId])
+
+  useEffect(() => {
+    if (selectedUser.followers) {
+      setIsFollowing(selectedUser.followers.includes(LOGGED_USER_ID))
+    }
+  }, [selectedUser.followers])
+
+  return (
+    <div style={MODAL_OVERLAY_STYLE}>
+      <div style={MODAL_BACKGROUND_STYLE}>
+        <div className="user-modal__close-button">
+          <Button onClick={handleCloseModal}>&#x2715;</Button>
+        </div>
+        <div className="user-modal__header">
+          <img src={selectedUser.url} alt={selectedUser.name} className="user-modal__image" />
+          <h1 className="user-modal__username">
+            {selectedUser.username} ({selectedUser.name})
+          </h1>
+          <p className="user-modal__membership-date">Joined Posterr at {selectedUser.membershipDate}</p>
+          {selectedUser.followers && <p className="user-modal__followers">{selectedUser.followers.length} followers</p>}
+          {selectedUser.following && <p className="user-modal__following">{selectedUser.following.length} following</p>}
+          {/* <div className="user-modal__posts">
+            {POSTS.map((post) => {
+              console.log('## userId inside POSTS', userId)
+              console.log('## post.userId inside POSTS', post.userId)
+              if (post.userId === userId) {
+                return <p key={post.id}>{post.postMessage}</p>
+              }
+            })}
+          </div> */}
+        </div>
+        {LOGGED_USER_ID !== selectedUser.id && (
+          <div className="user-modal__status">
+            <p className="user-modal__following-status">
+              {`${isFollowing ? 'You are' : 'You are not'}`} following {selectedUser.username}
+            </p>
+            <Button onClick={handleIsFollowing}>{`${isFollowing ? 'UNFOLLOW' : 'FOLLOW'}`}</Button>
+          </div>
+        )}
+        <TextArea onClick={handlePost} />
+      </div>
     </div>
-    <div>{children}</div>
-  </ReactModal>
-)
-
-Modal.propTypes = {
-  isOpen: PropTypes.bool.isRequired,
-  onClose: PropTypes.func,
-  children: PropTypes.node,
-}
-
-Modal.defaultProps = {
-  onClose: () => {},
-  children: null,
+  )
 }
 
 export default Modal
---
+++ src/routes/layout/index.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { Link, Outlet } from 'react-router-dom'
+
+import SwitchButton from '../../components/button/switch-button'
+import Profile from '../../components/profile/index'
+import { POSTS_VIEW_TYPE } from '../../utils/constants'
+import logoColor from '../../assets/logo-color.png'
+import TextArea from '../../components/text-area'
+import USERS from '../../users-data.json'
+
+import './styles.css'
+
+const Layout = () => {
+  const [loggedUser] = useState(USERS[0])
+  const [isChecked, setIsChecked] = useState(false)
+
+  const onSwitchButtonChange = (event) => {
+    const { checked } = event.target
+    setIsChecked(checked)
+    // if (isChecked) {
+    //   navigate('/all.')
+    // } else {
+    //   navigate('/following')
+    // }
+  }
+
+  return (
+    <div className="layout">
+      {/* <img src={logoColor} alt="Posterr logo" className="layout-logo" />
+      <h1 className="layout-title">Posterr</h1>
+      <SwitchButton checked={isChecked} onChange={onSwitchButtonChange} name="switch-button" label={isChecked ? POSTS_VIEW_TYPE.FOLLOWING : POSTS_VIEW_TYPE.ALL} />
+      <Profile user={loggedUser} /> */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">All</Link>
+          </li>
+          <li>
+            <Link to="/following">Following</Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+      <Outlet />
+      <div className="layout-text-area">
+        <TextArea />
+      </div>
+    </div>
+  )
+}
+
+Layout.propTypes = {
+  user: PropTypes.shape({}),
+}
+
+Layout.defaultProps = {
+  user: {},
+}
+
+export default Layout
